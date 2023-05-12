026380cb162ba0e51153dab13f20685a7436587b
muriloabreu <murilo.lilo@hotmail.com>
2023-05-12 17:58:11 -0300

    Inicialização de API Spring boot

---
+++ src/main/java/com/api/calculamva/CalculaMvaApplication.java
@@ -0,0 +1,13 @@
+package com.api.calculamva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculaMvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculaMvaApplication.class, args);
+	}
+
+}
