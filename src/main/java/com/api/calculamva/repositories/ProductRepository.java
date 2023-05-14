4daf849879b0660d21dd741af70d6c162a8bc4cc
muriloabreu <murilo.lilo@hotmail.com>
2023-05-14 20:32:09 -0300

    Criação do package Repository com a Interface ProductRepository

---
+++ src/main/java/com/api/calculamva/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.api.calculamva.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.calculamva.models.ProductModel;
+
+public interface ProductRepository extends JpaRepository<ProductModel, Long>{
+
+}
