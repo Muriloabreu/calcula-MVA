dc02200838658b97f29d1d2e8191971abd025481
muriloabreu <murilo.lilo@hotmail.com>
2023-05-15 22:14:24 -0300

    Implementação de metodo existsByName na interface ProductService

--- src/main/java/com/api/calculamva/repositories/ProductRepository.java
+++ src/main/java/com/api/calculamva/repositories/ProductRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.api.calculamva.models.ProductModel;
 
 public interface ProductRepository extends JpaRepository<ProductModel, Long>{
+	
+	boolean existsByName(String name);
 
 }
