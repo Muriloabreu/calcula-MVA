a8bbc6b770f58a806e3a13a005190955e77ea3d3
muriloabreu <murilo.lilo@hotmail.com>
2023-05-17 17:16:53 -0300

    Criação de classe TaxationServiceImpl

---
+++ src/main/java/com/api/calculamva/services/TaxationServiceImpl.java
@@ -0,0 +1,46 @@
+package com.api.calculamva.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.calculamva.models.TaxationModel;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class TaxationServiceImpl implements TaxationRepository{
+	
+	@Autowired
+	TaxationRepository taxationRepository;
+
+	@Override
+	public List<TaxationModel> findAll() {
+		
+		return taxationRepository.findAll();
+	}
+
+	@Override
+	public Optional<TaxationModel> findById(Long id) {
+		
+		return taxationRepository.findById(id);
+	}
+
+	@Override
+	@Transactional
+	public TaxationModel save(TaxationModel taxation) {
+		
+		return taxationRepository.save(taxation);
+	}
+
+	@Override
+	@Transactional
+	public void delete(TaxationModel taxation) {
+		
+		taxationRepository.delete(taxation);
+		
+	}
+
+}
