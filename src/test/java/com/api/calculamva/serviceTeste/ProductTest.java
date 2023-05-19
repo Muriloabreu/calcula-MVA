f0b54fa8738a989438c297022d97d21ba4cadf98
muriloabreu <murilo.lilo@hotmail.com>
2023-05-18 21:45:19 -0300

    Criação de classe para teste unitario JUnit classe teste Product

---
+++ src/test/java/com/api/calculamva/serviceTeste/ProductTest.java
@@ -0,0 +1,13 @@
+package com.api.calculamva.serviceTeste;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
+public class ProductTest {
+
+}
