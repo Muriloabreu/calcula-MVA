9ec924508a148bf31c93f2aec4d39f9c7fdee289
muriloabreu <murilo.lilo@hotmail.com>
2023-05-19 18:43:09 -0300

    Implementação de classe test TaxationTest

--- src/test/java/com/api/calculamva/serviceTeste/TaxationTest.java
+++ src/test/java/com/api/calculamva/serviceTeste/TaxationTest.java
@@ -1,5 +1,15 @@
 package com.api.calculamva.serviceTeste;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
 public class TaxationTest {
+	
+	Taxa
 
 }
