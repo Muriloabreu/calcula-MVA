8b6e6a51a21fa4ac67f4dde43643a64e53843269
muriloabreu <murilo.lilo@hotmail.com>
2023-05-21 20:52:34 -0300

    Implementação de testes

--- src/test/java/com/api/calculamva/serviceTeste/TaxationTest.java
+++ src/test/java/com/api/calculamva/serviceTeste/TaxationTest.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import javax.print.attribute.standard.DateTimeAtCompleted;
 
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,7 +26,8 @@ public class TaxationTest {
 	@Autowired
 	TaxationService ts;
 	
-	@Test
+	//@Test
+	@Ignore
 	public void save() {
 	
 		
@@ -33,7 +35,7 @@ public class TaxationTest {
 	
 	state.setName("Pernambuco");
 	state.setAbbreviation("PE");
-	state.setIcms(18);
+	state.setIcms(18.0);
 	
 	ProductModel p01 = new ProductModel();
 
@@ -48,9 +50,32 @@ public class TaxationTest {
 	taxa1.setState(state);
 	taxa1.setAmount(10);
 	
+	taxa1.setCalculoMva(taxa1.getCalculoMva());
+	taxa1.setValueMva(taxa1.getValueMva());
+	taxa1.setIcmsXvalueMva(taxa1.getIcmsXvalueMva());
+	taxa1.setIcmsXicms(taxa1.getIcmsXicms());
+	taxa1.setUND(taxa1.getUND());
+	taxa1.setValueXicms(taxa1.getValueXicms());
+	taxa1.setTotalNFE(taxa1.getTotalNFE());
 	
+	
+	System.out.println();
 	System.out.println(taxa1);
-	
+	System.out.println();
+	System.out.println();
+	System.out.println("Valor R$: " + taxa1.getProduct().getUnitaryValue() );
+	System.out.println("ICMS State " + taxa1.getState().getIcms() + "%: " + taxa1.getIcmsXProduct());
+	System.out.println("MVA :" + taxa1.getMVA() + "%" );
+	System.out.println("Calculo MVA: " + taxa1.getCalculoMva());
+	System.out.println("Valor + MVA: " + taxa1.getValueMva());
+	System.out.println("ICMS: "+ taxa1.getState().getIcms() + "% : " + taxa1.getIcmsXvalueMva());
+	System.out.println("ICMS " + taxa1.getState().getIcms() + "% - " + taxa1.getState().getIcms() + "%: " + taxa1.getIcmsXicms());
+	System.out.println("UND: " + taxa1.getUND());
+	System.out.println("Valor + ICMS: " + taxa1.getValueXicms());
+	System.out.println("Total da NFE: " + taxa1.getTotalNFE());
+	System.out.println("=================");
+	System.out.println();
+	System.out.println();
 	}
 
 }
